8f013d0127eb667f0df0375535e3d46727021807
ChidsX <131833639+ChidsX@users.noreply.github.com>
2025-01-23 13:55:08 +0000

    "added Statistics"

---
+++ student_management_project/Statistics.py
@@ -0,0 +1,11 @@
+
+marks= = 
+
+def averages(marks):
+    for number in marks:
+        added = added + number
+        count =+ 1
+    
+    answer = added / count
+    print{f"The average of the students Marks is {answer}"}
+    
